9e721dcf0512674ef7686585de3532fb31690af7
Shadowissic <68826565+Shadowissic@users.noreply.github.com>
2020-07-27 17:44:52 -0400

    first push with functions

---
+++ commands/oldest.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js")
+const bot = new Discord.Client();
+
+module.exposrts.eun = async (bot, message, args) => {
+
+    if(!message.guild) return message.channel.send(`This command can only be used in a server.`)
+
+    let mem = message.guild.members.cache
+    .filter((m) => !m.user.bot)
+    .sort((a, b) => a.user.createdAt - b.user.createdAt)
+    .first();
+
+    const embed = new Discord.MessageEmbed()
+    .setColor(`#FF00FF`)
+    .setTitle(`Oldest Member in ${message.guild.name}!`)
+    .setDescription(`Oldest Member: **${mem.user.tag}** `)
+    message.channel.send(embed)
+}
+
+module.exports.help = {
+    name: 'oldest',
+    aliases: []
+}
---
+++ functions.js
@@ -0,0 +1,6 @@
+module.exports = {
+    calculator: function (num1, operatior, num2) {
+        if(!num1) throw new TypeError(`Missing number 1 :(`)
+        if(!operator) throw new TypeError(`Missing Second number :(`)
+    }
+}
