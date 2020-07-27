be7b567f98edebb2d44094029c86936d8757fd32
Shadowissic <68826565+Shadowissic@users.noreply.github.com>
2020-07-27 18:06:48 -0400

    Update math.js

--- commands/math.js
+++ commands/math.js
@@ -4,10 +4,10 @@ const bot = new Discord.Client();
 module.exports.run = async (bot, message, args) => {
 
     const reason = args.join(" ")
-    if(Response.length < 1) return message.channel.send("Please provide an expression after the command.")
+    if(reason.length < 1) return message.channel.send("Please provide an expression after the command.")
 
     try {
-        const solved = evaluate(reason).toString();
+        const solved = eval(reason).toString();
         let calembed = new Discord.MessageEmbed()
         .setColor(`#ff8400`)
         .setTitle(`Math!`)
