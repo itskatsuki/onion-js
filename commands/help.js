b126959a873ac383122df45e5e0d207f72d0f553
Shadowissic <68826565+Shadowissic@users.noreply.github.com>
2020-07-30 23:08:21 -0400

    Revert "Update help.js"
    This reverts commit 796e71201129d4ef3d001a02cd773d0ceed159ba.

--- commands/help.js
+++ commands/help.js
@@ -7,8 +7,9 @@ module.exports.run = async (client, message, args) => {
   .addField(`Prefix:`, prefix)
   .addField(`Fun:`, `\`\`\`${prefix}meme, ${prefix}snipe, ${prefix}tweet, ${prefix}meme, ${prefix}cleanmeme, ${prefix}8ball, ${prefix}dice, ${prefix}reddit, ${prefix}joke, ${prefix}coinflip, ${prefix}nou, ${prefix}woah, ${prefix}dumbrate, ${prefix}monkey\`\`\``)
   .addField(`Moderation:`, `\`\`\`${prefix}clear, ${prefix}ban, ${prefix}kick\`\`\``)
+  .addField(`Music`, `\`\`\`${prefix}play, ${prefix}stop\`\`\``)
   .addField(`Information:`, `\`\`\`${prefix}help, ${prefix}math, ${prefix}avtar, ${prefix}ping, ${prefix}servercount, ${prefix}about\`\`\``)
-  .addField(`Other:`, `\`\`\`${prefix}status, ${prefix}suggest, ${prefix}uptime, ${prefix}poll\`\`\``)
+  .addField(`Other:`, `\`\`\`${prefix}status, ${prefix}suggest, ${prefix}uptime\`\`\``)
   message.channel.send(helpEmbed)
 }
 
