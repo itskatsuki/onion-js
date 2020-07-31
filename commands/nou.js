5e6dcbef6e7f667e9f4e273aaaa57e0e4f593906
Shadowissic <68826565+Shadowissic@users.noreply.github.com>
2020-07-30 23:05:22 -0400

    Update nou.js

--- commands/nou.js
+++ commands/nou.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
       const embed = new Discord.MessageEmbed()
     .setColor(`#FF00FF`)
     .setTitle(`${message.author.username} just pulled out an Uno reverse card!`)
-    .setImage("https://media.giphy.com/media/Wt6kNaMjofj1jHkF7t/so")
+    .setImage("https://media.giphy.com/media/Wt6kNaMjofj1jHkF7t/source.gif")
 
     message.channel.send({embed})
 }
